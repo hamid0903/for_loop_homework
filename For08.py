50197f802ad5191928bae8c963d38e1c17d3b8a1
Hamid Kamalov <hamid_kamalov@mail.ru>
2022-01-18 22:58:33 +0500

    for08-2

--- For08.py
+++ For08.py
@@ -1,3 +1,6 @@
+from re import X
+
+
 def main(N):
     """
     The number N is given. Calculate the sum below: 1 + 1/2 + 1/3 + … + 1/N.
@@ -6,4 +9,11 @@ def main(N):
     Returns:
         float: return  answer
     """
-    return
+    idx=0
+    N+=1
+    for i in range(1,N):
+        idx+=1/i
+    return idx
+print(main(5))
+x=1+1/2+1/3+1/4+1/5
+print(x)
